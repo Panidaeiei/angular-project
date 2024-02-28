d9203f7c89e199a9832ba52adb640e5fecdf7035
Panidaeiei <161023725+Panidaeiei@users.noreply.github.com>
2024-02-29 00:02:29 +0700

    create signup page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './project/login/login.component';
+import { SignupComponent } from './project/signup/signup.component';
 
 export const routes: Routes = [
-    {path: '' , component : LoginComponent}
+    {path: 'login' , component : LoginComponent},
+    {path: '' , component : SignupComponent}
 ];
---
+++ src/app/project/signup/signup.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [RouterOutlet,
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent {
+
+}
