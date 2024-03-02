ab11b287b9fe3239f1209dc605530fd19a07b006
SananTongchot <161601715+SananTongchot@users.noreply.github.com>
2024-03-02 16:22:40 +0700

    nomal

--- src/app/app.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import bodyParser from "body-parser";
-import { router as cat } from "./api/cat";
-
-
-export const app = express();
-app.use(bodyParser.text());
-app.use(bodyParser.json());
-app.use("/", cat);
