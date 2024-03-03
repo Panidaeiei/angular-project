50fca3064ea1909b337a5ac91e7e6305ae742460
Panidaeiei <161023725+Panidaeiei@users.noreply.github.com>
2024-03-03 17:44:24 +0700

    create-pagetop

--- src/app/project/pagetop/pagetop.component.ts
+++ src/app/project/pagetop/pagetop.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-pagetop',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    RouterModule,
+    MatInputModule],
   templateUrl: './pagetop.component.html',
   styleUrl: './pagetop.component.scss'
 })
