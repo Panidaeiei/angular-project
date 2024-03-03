99bce96396bba408157d6971c552f9fb9b11ee05
SananTongchot <161601715+SananTongchot@users.noreply.github.com>
2024-03-04 02:45:51 +0700

    checklogin

--- src/app/project/login/login.component.ts
+++ src/app/project/login/login.component.ts
@@ -6,7 +6,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -19,9 +22,33 @@ import { RouterModule, Routes } from '@angular/router';
     MatIconModule,
     MatListModule,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    RouterLink,
+    HttpClientModule,
+    FormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor(private http: HttpClient, private router: Router) {}
+
+  name: any = '';
+  password: any = '';
+  resultCheck = false;
+
+  check() {
+    let queryParams = `?name=${encodeURIComponent(
+      this.name
+    )}&password=${encodeURIComponent(this.password)}`;
+
+    this.http
+      .get(`http://localhost:3000${queryParams}`)
+      .subscribe((result: any) => {
+        console.log(result);
+        if (result.length > 0) {
+          this.router.navigate(['/user']);
+        }
+      });
+  }
+}
