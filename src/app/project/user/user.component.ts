493618c75c2dba0a0ee9def4a116758c72c7d2cb
SananTongchot <161601715+SananTongchot@users.noreply.github.com>
2024-03-04 23:54:58 +0700

    elo2

--- src/app/project/user/user.component.ts
+++ src/app/project/user/user.component.ts
@@ -4,7 +4,8 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { RouterModule} from '@angular/router';
+import { RouterModule,Router,RouterLink} from '@angular/router';
+import { HttpClient,HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -14,11 +15,25 @@ import { RouterModule} from '@angular/router';
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    RouterModule,
+    RouterModule,HttpClientModule,RouterLink
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+  Catresult:any=[];
+  constructor(private router: Router , private http:HttpClient) {
+     this.Catdata();
+  }
 
+  Catdata() {
+    this.http.get(`http://localhost:3000/random`).subscribe((result: any) => {
+      console.log(result);
+      this.Catresult = result ;
+      
+    });
+  }
+  link(){
+    this.router.navigate(["/user"]);
+  }
 }
