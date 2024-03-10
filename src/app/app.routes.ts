4e0d67496b8053b78ef54c481b47cc1f42254597
Panidaeiei <161023725+Panidaeiei@users.noreply.github.com>
2024-03-10 22:49:03 +0700

    pani-font

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ import { UserComponent } from './project/user/user.component';
 import { ProfileUserComponent } from './project/profile-user/profile-user.component';
 import { EditProfileuserComponent } from './project/edit-profileuser/edit-profileuser.component';
 import { PagetopComponent } from './project/pagetop/pagetop.component';
+import { EditimgComponent } from './project/editimg/editimg.component';
+import { ImgrankingComponent } from './project/imgranking/imgranking.component';
+import { PreimgrankComponent } from './project/preimgrank/preimgrank.component';
 
 export const routes: Routes = [
     {path: 'login' , component : LoginComponent},
@@ -14,5 +17,8 @@ export const routes: Routes = [
     {path: 'user', component : UserComponent},
     {path: 'profileuser', component : ProfileUserComponent},
     {path: 'editpro', component : EditProfileuserComponent},
-    {path: 'top10' , component : PagetopComponent}
+    {path: 'top10' , component : PagetopComponent},
+    {path: 'editimg' , component : EditimgComponent },
+    {path: 'imgrank' , component : ImgrankingComponent },
+    {path: 'preimg' , component : PreimgrankComponent }
 ];
